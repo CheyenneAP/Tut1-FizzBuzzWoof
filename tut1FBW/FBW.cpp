70546fde560e67907856a8926d06eba1d23e4706
Cheyenne Pillay <Cheyenneivan@gmail.com>
2016-03-03 22:41:45 +0200

    commit 5: testing the first function to check divisibity of  first 20 numbers

--- tut1FBW/FBW.cpp
+++ tut1FBW/FBW.cpp
@@ -8,7 +8,7 @@ class FizzBuzzWolf
 private:
 	// not accessible by the user
 	int number;
-	int c;
+	int flag;
 public:
 	// accessible to methods 
 	void setNumValues(void);
@@ -20,10 +20,48 @@ public:
 // default contructor 
 void FizzBuzzWolf::setNumValues(){
 	number = 1;
-	c = 0;
+	flag = 0;
 }
 
+void FizzBuzzWolf::funct1(){
+	// numbers must be less than and equal to 20
+	while (number <= 20){
 
+		if (number % 3 == 0){
+			// intro of flag to avoid repetitive code
+			flag++;
+		}
+
+
+		if (number % 5 == 0){
+			flag = flag + 2;
+		}
+
+		else{
+			cout << number << endl;
+		}
+
+		if (flag == 1){
+			cout << number << " is divisible by 3\t" << "Fizz" << endl;
+		}
+
+		if (flag == 2){
+			cout << number << " is divisible by 5\t" << "Buzz" << endl;
+		}
+
+		if (flag == 3){
+			cout << number << " is divisible by 3 and 5\t" << "FizzBuzz" << endl;
+		}
+
+		// move to the next number in the loop
+		number++;
+		flag = 0;
+
+
+	}
+};
+
+/*
 
 void fizzbuzz(int x){
 	if (x % 3 == 0){
@@ -66,6 +104,17 @@ int main(){
 		fizzbuzz(num);
 		num++;
 	}
+
+
+}
+*/
+
+int main()
+{
+	FizzBuzzWolf myfuzzOBj;
+	myfuzzOBj.setNumValues();
+	myfuzzOBj.funct1();
+	
 
 
 }
