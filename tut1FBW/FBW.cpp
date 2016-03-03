b31fc5f207748a971555fadecb8e2f3d6b4e15f9
Cheyenne Pillay <Cheyenneivan@gmail.com>
2016-03-03 22:42:48 +0200

    commit 6: test function to see if it works

--- tut1FBW/FBW.cpp
+++ tut1FBW/FBW.cpp
@@ -61,53 +61,7 @@ void FizzBuzzWolf::funct1(){
 	}
 };
 
-/*
 
-void fizzbuzz(int x){
-	if (x % 3 == 0){
-		cout << "Fizz" << endl;
-
-	}
-	if (x % 5 == 0){
-		cout << "Buzz" << endl;
-
-	}
-	if (x % 7 == 0){
-		cout << "Woof" << endl;
-
-	}
-	if (x % 3 == 0 && x % 5 == 0){
-		cout << "FizzBuzz" << endl;
-
-	}
-	if (x % 5 == 0 && x % 7 == 0){
-		cout << "BuzzWoof" << endl;
-	}
-	if (x % 3 == 0 && x % 7 == 0){
-		cout << "FizzWoof" << endl;
-	}
-
-	if (x % 3 == 0 && x % 5 == 0 && x % 7 == 0){
-		cout << "FizzBuzzWoof" << endl;
-	}
-	else{
-		cout << "not divisible by 3 ,5 and 7" << endl;
-	}
-}
-
-
-
-int main(){
-	int num = 1;
-	while (num <= 50)
-	{
-		fizzbuzz(num);
-		num++;
-	}
-
-
-}
-*/
 
 int main()
 {
